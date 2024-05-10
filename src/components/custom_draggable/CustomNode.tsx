e1b70c56af8a794b81bf53d654e3b718a1620d5d
anhhuy <phamanhhuy22@gmail.com>
2024-05-10 22:04:56 +0700

    change layout show sub node

--- src/components/custom_draggable/CustomNode.tsx
+++ src/components/custom_draggable/CustomNode.tsx
@@ -29,14 +29,18 @@ export const CustomNode: React.FC<Props> = (props) => {
 
         setIsProcessing(true);
 
-        props.onToggle(props.node.id);
+        if(props.node.data && !props.node.data.is_area) {
+            props.node.droppable = false;
+        }
 
+        props.onToggle(props.node.id);
         setTimeout(() => {
             setIsProcessing(false);
         }, 200);
     };
 
     const dragOverProps = useDragOver(id, props.isOpen, props.onToggle); //auto expand on dragover
+
     return (
         <div style={{paddingInlineStart: indent}}>
             <div
@@ -44,11 +48,15 @@ export const CustomNode: React.FC<Props> = (props) => {
                 className={`flex items-center gap-x-2 mt-2 cursor-pointer no-select`}
                 {...dragOverProps}
             >
-
-                {data && data.is_area ? <FolderIcon/> : <LocationIcon/>}
-
-                {data && data.label}
-                {droppable &&
+               <div>
+                   <div className={"flex items-center gap-x-2"}>
+                       {data && data.is_area ? <FolderIcon/> : <LocationIcon/>}
+                       {data && data.label}
+                   </div>
+                   {droppable && data && !data.is_area &&
+                       <p className={"text-center hover:underline text-gray-500 text-sm mt-2"}>View more</p>}
+               </div>
+                {droppable && data && data.is_area &&
                     <UpIcon className={twMerge(`transition-all duration-200`, !props.isOpen && `rotate-180`)}/>}
                 </div>
         </div>
